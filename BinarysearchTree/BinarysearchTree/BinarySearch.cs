a57fcc77532dc030cb8e7c4316d678709ccc08d0
Rajangane <pratiksharajangane7@gmail.com>
2021-12-13 20:43:48 +0530

    [Pratiksha] ADD : Creating BST

---
+++ BinarysearchTree/BinarysearchTree/BinarySearch.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarysearchTree
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+        public T Data { get; set; }
+        public BinaryTree<T> LTree { get; set; }
+        public BinaryTree<T> RTree { get; set; }
+        public BinaryTree(T data) //constructor
+        {
+            this.Data = data;
+            this.RTree = null; //RightTree
+            this.LTree = null; //LeftTree
+        }
+
+        /// <summary>
+        /// 
+        /// ADD Method
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            T currentNodeValue = this.Data;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LTree == null)
+                    this.LTree = new BinaryTree<T>(item);
+                else
+                    this.LTree.Add(item);
+            }
+            else
+            {
+                if (this.RTree == null)
+                    this.RTree = new BinaryTree<T>(item);
+                else
+                    this.RTree.Add(item);
+            }
+
+        }
+        private int LCount = 0, RCount = 0; //LeftCount, RightCount
+        /// <summary>
+        /// Display Method
+        /// </summary>
+        public void Display()
+        {
+            if (this.LTree != null)
+            {
+                this.LCount++;
+                this.LTree.Display();
+
+            }
+            Console.WriteLine(this.Data.ToString());
+            if (this.RTree != null)
+            {
+                this.RCount++;
+                this.RTree.Display();
+            }
+        }
+    }
+}
--- BinarysearchTree/BinarysearchTree/Program.cs
+++ BinarysearchTree/BinarysearchTree/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("************* Welcome To BST **************");
+//Console.WriteLine("************* Welcome To BST **************");
+using BinarysearchTree;
+Console.Write("Select Number:\n1)CreateBST");
+int option = Convert.ToInt32(Console.ReadLine());
+
+switch (option)
+{
+    case 1:
+        BinaryTree<int> binarySearch = new BinaryTree<int>(56);
+        binarySearch.Add(30);
+        binarySearch.Add(70);
+        binarySearch.Display();
+        break;
+}
